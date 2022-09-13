f7611620e3dec2d128bbdf18c64159f527719dde
Justin Payne <crashfrog@gmail.com>
2022-09-13 15:30:42 +0700

    corrected pin definitions

--- lib/pimoroni_physical_feather_pins/feathers2.py
+++ lib/pimoroni_physical_feather_pins/feathers2.py
@@ -3,25 +3,25 @@ from . import pin_error
 import microcontroller
 
 def pin3():
-    return microcontroller.pin.GPIO0
+    raise NotImplementedError
 
 def pin5():
-    return microcontroller.pin.GPIO17
-
-def pin6():
     return microcontroller.pin.GPIO18
 
+def pin6():
+    return microcontroller.pin.GPIO17
+
 def pin7():
-    return microcontroller.pin.GPIO13
+    return microcontroller.pin.GPIO16
 
 def pin8():
-    return microcontroller.pin.GPIO12
+    return microcontroller.pin.GPIO15
 
 def pin9():
-    return microcontroller.pin.GPIO6
+    return microcontroller.pin.GPIO14
 
 def pin10():
-    return microcontroller.pin.GPIO5
+    return microcontroller.pin.GPIO8
 
 def pin11():
     return microcontroller.pin.GPIO36
@@ -33,41 +33,41 @@ def pin13():
     return microcontroller.pin.GPIO37
 
 def pin14():
-    return microcontroller.pin.GPIO44
+    return microcontroller.pin.GPIO38
 
 def pin15():
-    return microcontroller.pin.GPIO43
+    return microcontroller.pin.GPIO39
 
 def pin16():
 #   This is 3V3 LDO 2 OUTPUT
     raise NotImplementedError
 
 def pin17():
-    return microcontroller.pin.GPIO8
-
-def pin18():
-    return microcontroller.pin.GPIO9
-
-def pin19():
-    return microcontroller.pin.GPIO33
-
-def pin20():
-    return microcontroller.pin.GPIO38
-
-def pin21():
-    return microcontroller.pin.GPIO1
-
-def pin22():
     return microcontroller.pin.GPIO3
 
-def pin23():
-    return microcontroller.pin.GPIO7
+def pin18():
+    return microcontroller.pin.GPIO4
 
-def pin24():
+def pin19():
+    return microcontroller.pin.GPIO5
+
+def pin20():
+    return microcontroller.pin.GPIO6
+
+def pin21():
+    return microcontroller.pin.GPIO9
+
+def pin22():
     return microcontroller.pin.GPIO10
 
-def pin25():
+def pin23():
     return microcontroller.pin.GPIO11
+
+def pin24():
+    return microcontroller.pin.GPIO12
+
+def pin25():
+    return microcontroller.pin.GPIO13
 
 def init(scope):
     """Pull the pin definitions into the main module namespace"""
